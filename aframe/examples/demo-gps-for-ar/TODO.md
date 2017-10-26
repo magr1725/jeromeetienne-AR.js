502d433341a9914501ff34db2e48a26fa36695e3
Jerome Etienne <jerome.etienne@gmail.com>
2017-10-26 14:10:36 +0100

    experiment with gps and ar

---
+++ aframe/examples/demo-gps-for-ar/TODO.md
@@ -0,0 +1,8 @@
+- put the aframe components in their own source
+- camera initialisation isnt compatible with marker projection matrix
+- make a good example
+- make a debug layer
+  - gps-debug on camera
+
+- clean up the code
+- remove abreviation
---
+++ aframe/examples/demo-gps-for-ar/src/gps-ar.js
@@ -0,0 +1,399 @@
+//////////////////////////////////////////////////////////////////////////////
+//		component gps-position
+//////////////////////////////////////////////////////////////////////////////
+
+AFRAME.registerComponent('gps-position', {
+	
+	_watchPositionId: null,
+	zeroCrd: null,
+	crd: null,
+	
+	schema: {
+		accuracy: {
+			type: 'int',
+			default: 100
+		},
+		'zero-crd-latitude': {
+			type: 'number',
+			default: NaN
+		},
+		'zero-crd-longitude': {
+			type: 'number',
+			default: NaN
+		}
+	},
+	
+	init: function () {
+		
+		if(!isNaN(this.data['zero-crd-latitude']) && !isNaN(this.data['zero-crd-longitude'])){
+			this.zeroCrd = {latitude: this.data['zero-crd-latitude'], longitude: this.data['zero-crd-longitude']}
+		}
+		
+		this._watchPositionId = this.watchGPS(function(position){
+			this.crd = position.coords
+			this.updatePosition()
+		}.bind(this))
+		
+	},
+	
+	watchGPS: function (success, error) {
+		// TODO put that in .init directly
+
+		if( error === undefined ){
+			error = function(err) { console.warn('ERROR('+err.code+'): '+err.message) }			
+		}
+		
+		if( "geolocation" in navigator === false ){
+			error({code: 0, message: 'Geolocation is not supported by your browser'})
+			return
+		}
+		
+		return navigator.geolocation.watchPosition(success, error, {
+			enableHighAccuracy: true,
+			maximumAge: 0,
+			timeout: 27000
+		})
+	},
+	
+	updatePosition: function () {
+		
+		if( this.crd.accuracy > this.data.accuracy )	return
+		
+		if( this.zeroCrd === null ) this.zeroCrd = this.crd
+		
+		var position = this.el.getAttribute('position')
+		
+		// compute position.x
+		position.x = this.calcMeters(
+			this.zeroCrd,
+			{
+				longitude: this.crd.longitude,
+				latitude: this.zeroCrd.latitude
+			}
+		) * (
+			this.crd.longitude > this.zeroCrd.longitude
+				? 1 : -1
+		)
+		
+		// compute position.z
+		position.z = this.calcMeters(
+			this.zeroCrd,
+			{
+				longitude: this.zeroCrd.longitude,
+				latitude: this.crd.latitude
+			}
+		) * (
+			this.crd.latitude > this.zeroCrd.latitude
+				? -1 : 1
+		)
+		
+		// update element position
+		this.el.setAttribute('position', position)
+	},
+	
+	calcMeters: function(src, dest) {
+		var dlon = THREE.Math.degToRad(dest.longitude - src.longitude)
+		var dlat = THREE.Math.degToRad(dest.latitude - src.latitude)
+		
+		var a = (Math.sin(dlat / 2) * Math.sin(dlat / 2)) + Math.cos(THREE.Math.degToRad(src.latitude)) * Math.cos(THREE.Math.degToRad(dest.latitude)) * (Math.sin(dlon / 2) * Math.sin(dlon / 2))
+		var angle = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+		
+		return angle * 6378160
+	},
+	
+	remove: function() {
+		if(this._watchPositionId) navigator.geolocation.clearWatch(this._watchPositionId)
+		this._watchPositionId = null
+	}
+	
+})
+
+
+//////////////////////////////////////////////////////////////////////////////
+//		component compass-rotation 
+//////////////////////////////////////////////////////////////////////////////
+
+AFRAME.registerComponent('compass-rotation', {
+	
+	lookControls: null,
+	lastTimestamp: 0,
+	heading: null,
+	
+	
+	schema: {
+		fixTime: {
+			type: 'int',
+			default: 2000
+		},
+		orientationEvent: {
+			type: 'string',
+			default: 'auto'
+		}
+	},
+	
+	init: function () {
+		
+		if(typeof(this.el.components['look-controls']) == 'undefined') return
+		
+		this.lookControls = this.el.components['look-controls']
+		
+		this.handlerOrientation = this.handlerOrientation.bind(this)
+		
+		if(this.data.orientationEvent === 'auto'){
+			if('ondeviceorientationabsolute' in window){
+				this.data.orientationEvent = 'deviceorientationabsolute'
+			}else if('ondeviceorientation' in window){
+				this.data.orientationEvent = 'deviceorientation'
+			}else{
+				this.data.orientationEvent = ''
+				alert('Compass not supported')
+				return
+			}
+		}
+		
+		window.addEventListener( this.data.orientationEvent, this.handlerOrientation, false)
+		
+		window.addEventListener('compassneedscalibration', function(event) {
+			alert('Your compass needs calibrating! Wave your device in a figure-eight motion')
+			event.preventDefault()
+		}, true)
+		
+	},
+	
+	tick: function (time, timeDelta) {
+		
+		if(this.heading === null || this.lastTimestamp > (time - this.data.fixTime)) return
+		
+		this.lastTimestamp = time
+		this.updateRotation()
+		
+	},
+	
+	computeCompassHeading: function (alpha, beta, gamma) {
+		
+		// Convert degrees to radians
+		var alphaRad = alpha * (Math.PI / 180)
+		var betaRad = beta * (Math.PI / 180)
+		var gammaRad = gamma * (Math.PI / 180)
+		
+		// Calculate equation components
+		var cA = Math.cos(alphaRad)
+		var sA = Math.sin(alphaRad)
+		var cB = Math.cos(betaRad)
+		var sB = Math.sin(betaRad)
+		var cG = Math.cos(gammaRad)
+		var sG = Math.sin(gammaRad)
+		
+		// Calculate A, B, C rotation components
+		var rA = - cA * sG - sA * sB * cG
+		var rB = - sA * sG + cA * sB * cG
+		var rC = - cB * cG
+		
+		// Calculate compass heading
+		var compassHeading = Math.atan(rA / rB)
+		
+		// Convert from half unit circle to whole unit circle
+		if(rB < 0) {
+			compassHeading += Math.PI
+		}else if(rA < 0) {
+			compassHeading += 2 * Math.PI
+		}
+		
+		// Convert radians to degrees
+		compassHeading *= 180 / Math.PI
+		
+		return compassHeading
+	},
+	
+	handlerOrientation: function( event ){
+		
+		var heading = null
+		
+		//console.log('device orientation event', event)
+		
+		if( event.webkitCompassHeading  !== undefined ){
+			
+			if(event.webkitCompassAccuracy < 50){
+				heading = event.webkitCompassHeading
+			}else{
+				console.warn('webkitCompassAccuracy is event.webkitCompassAccuracy')
+			}
+			
+		}else if( event.alpha !== null ){
+			if(event.absolute === true || event.absolute === undefined ) {
+				heading = this.computeCompassHeading(event.alpha, event.beta, event.gamma)
+			}else{
+				console.warn('event.absolute === false')
+			}
+		}else{
+			console.warn('event.alpha === null')
+		}
+		
+		this.heading = heading	
+	},
+	
+	updateRotation: function() {
+		
+		/*
+		camera.components["look-controls"].yawObject.rotation.y = THREE.Math.degToRad(
+			(
+				360
+				- camera.components["compass-rotation"].heading
+				- (
+					camera.getAttribute('rotation').y
+					- THREE.Math.radToDeg(camera.components["look-controls"].yawObject.rotation.y)
+				)
+			)
+			% 360
+		)
+		*/
+		
+		
+		var heading = 360 - this.heading
+		var camera_rotation = this.el.getAttribute('rotation').y
+		var yaw_rotation = THREE.Math.radToDeg(this.lookControls.yawObject.rotation.y)
+		
+		var offset = ( heading - ( camera_rotation - yaw_rotation ) ) % 360
+		
+		this.lookControls.yawObject.rotation.y = THREE.Math.degToRad(offset)
+		
+	},
+	
+	remove: function () {
+		if(this.data.orientationEvent){			
+			window.removeEventListener(this.data.orientationEvent, this.handlerOrientation, false)
+		}
+	}
+	
+})
+
+//////////////////////////////////////////////////////////////////////////////
+//		Component gps-place
+//////////////////////////////////////////////////////////////////////////////
+
+AFRAME.registerComponent('gps-place', {
+	
+	cameraGpsPosition: null,
+	deferredInitInterval: 0,
+	
+	schema: {
+		latitude: {
+			type: 'number',
+			default: 0
+		},
+		longitude: {
+			type: 'number',
+			default: 0
+		},
+		cameraSelector: {
+			type: 'string',
+			default: 'a-camera, [camera]'
+		}
+	},
+	
+	init: function () {
+		if(this.deferredInit()) return
+		this.deferredInitInterval = setInterval(this.deferredInit.bind(this), 1000)
+	},
+	
+	deferredInit: function () {
+		
+		if(!this.cameraGpsPosition){
+			var camera = document.querySelector(this.data.cameraSelector)
+			if(typeof(camera.components['gps-position']) == 'undefined') return
+			this.cameraGpsPosition = camera.components['gps-position']
+		}
+		
+		if(!this.cameraGpsPosition.zeroCrd) return
+		
+		this.updatePosition()
+		
+		clearInterval(this.deferredInitInterval)
+		this.deferredInitInterval = 0
+		
+		return true
+	},
+	
+	updatePosition: function() {
+		
+		var position = {x: 0, y: 0, z: 0}
+		
+		// update position.x
+		position.x = this.cameraGpsPosition.calcMeters(
+			this.cameraGpsPosition.zeroCrd,
+			{
+				longitude: this.data.longitude,
+				latitude: this.cameraGpsPosition.zeroCrd.latitude
+			}
+		) * (
+			this.data.longitude > this.cameraGpsPosition.zeroCrd.longitude
+				? 1 : -1
+		)
+		
+		// update position.z
+		position.z = this.cameraGpsPosition.calcMeters(
+			this.cameraGpsPosition.zeroCrd,
+			{
+				longitude: this.cameraGpsPosition.zeroCrd.longitude,
+				latitude: this.data.latitude
+			}
+		) * (
+			this.data.latitude > this.cameraGpsPosition.zeroCrd.latitude
+				? -1 : 1
+		)
+		
+		// update element's position
+		this.el.setAttribute('position', position)
+	}
+})
+
+//////////////////////////////////////////////////////////////////////////////
+//		Component gps-debug
+//////////////////////////////////////////////////////////////////////////////
+
+AFRAME.registerComponent('gps-debug', {
+	init : function(){
+		var camera = this.el;
+
+		// TODO cleanup this code
+		// TODO build the html element in there
+
+		camera.addEventListener('componentchanged', function (evt) {
+			switch(evt.detail.name){
+				case 'rotation':
+					//console.log('camera rotation changed', evt.detail.newData);
+					var compassRotation = camera.components['compass-rotation']
+					var lookControls = camera.components['look-controls']
+
+					camera_angle.innerText = evt.detail.newData.y;
+
+					if( lookControls ){
+						yaw_angle.innerText = THREE.Math.radToDeg(lookControls.yawObject.rotation.y);
+					}
+					if( compassRotation ){
+						compass_heading.innerText = compassRotation.heading;
+					}
+					break;
+				case 'position':
+					//console.log('camera position changed', evt.detail.newData);
+					camera_p_x.innerText = evt.detail.newData.x;
+					camera_p_z.innerText = evt.detail.newData.z;
+
+					var gpsPosition = camera.components['gps-position'];
+					if( gpsPosition ){
+						if(gpsPosition.crd){
+							crd_longitude.innerText = gpsPosition.crd.longitude;
+							crd_latitude.innerText = gpsPosition.crd.latitude;
+						}
+						if(gpsPosition.zeroCrd){
+							zero_crd_longitude.innerText = gpsPosition.zeroCrd.longitude;
+							zero_crd_latitude.innerText = gpsPosition.zeroCrd.latitude;
+						}
+					}
+					
+					break;
+			}
+		});
+		
+	}
+})
